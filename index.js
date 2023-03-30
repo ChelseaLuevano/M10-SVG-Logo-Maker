27aa079a8efa8834431f88078598c86e9581d673
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-03-30 17:58:02 -0400

    added pseudocode to index.js file and created svg.js file and examples folder

---
+++ index.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// An array of questions for user input
+
+
+// Function to generate a logo
+
+
+// Function to Write SVG File
+
+
+// Function to initialize app
+
+
+// Call Initialize App Function
