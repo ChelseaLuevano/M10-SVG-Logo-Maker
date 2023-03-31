fb808db81d04cdbcdbfb9538ec3f24c076b8621c
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-03-31 19:36:58 -0400

    after looking at mini challenge 28 example from M10, I decided to reorganize my functions by moving them within the lib folder and keeping index.js simple. This involved creating a cli file where most of my functions moved to, and creating a generate html .js file.

--- index.js
+++ index.js
@@ -1,73 +1,7 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const logoFile = 'logo.svg';
+const CLI = require('./lib/cli.js');
 
+// Defining the command line interface class
+const cli = new CLI();
 
-// An array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        message: 'Enter up to 3 characters for your logo text',
-        name: 'text',
-        validate: (value) => { 
-        if (value <= 3) { return true } 
-        else { return "Must be between 1-3 characters." } },
-    },
-    {
-        type: 'input',
-        message: 'Enter a text color',
-        name: 'textcolor',
-        validate: (value) => { 
-        if (value === '') { return true } 
-        else { return "Enter color text or 6 character hexadecimal numbers without hashtag" } },
-    },
-    {
-        type: 'list',
-        message: 'Pick your logo shape',
-        name: 'shape',
-        choices: [
-            "Circle",
-            "Triangle",
-            "Square"
-        ]
-    },
-    {
-        type: 'input',
-        message: 'Enter a shape color',
-        name: 'shapecolor',
-        validate: (value) => { 
-        if (value === '') { return true } 
-        else { return "Enter shape color or 6 character hexadecimal numbers without hashtag" } },
-    },
-]
-
-
-// Function to Write SVG File
-const writeToFile = function (data) {
-    fs.writeFile(logoFile, data, (err) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("The logo file has been created!")
-    });
-}
-
-
-// Function to initialize app
-function init() {
-    inquirer.prompt (questions)
-    .then(answers => {
-        const logoContent = generateLogo(answers);
-        writeToFile(logoContent);
-    })
-    .catch(err => {
-        console.log(err);
-    })
-}
-
-// Function to generate logo for svg 
-function generateLogo(answers) {
-
-}
-
-// Call Initialize App Function
+// Function to initialize app by calling the command line interface class
+cli.run();
