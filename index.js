614463fe95965e043d4231019cb300e5e8962344
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-04-01 15:43:26 -0400

    added render statements in square, triangle, and circle js files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const CLI = require('./lib/cli.js');
 
-// Defining the command line interface class
+// Defining the command line interface class varac
 const cli = new CLI();
 
 // Function to initialize app by calling the command line interface class
